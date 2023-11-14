8449d4e9aae5f1bfd2254f3b6764021656511d63
ragan <raganalemu3@gmail.com>
2023-11-14 12:46:53 +0300

    Add component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        <View style={styles.items}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +15,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EBEAED",
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {},
 });
---
+++ components/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Task = () => {
+  return (
+    <View>
+      <Text>this is a task</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default Task;
